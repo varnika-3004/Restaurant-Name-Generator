ee0d15796e40eac9f85c8e941637ca7a9e51efcd
Varnika Bansal <varnikabansal@gmail.com>
2023-11-28 14:11:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import langchain_helper 
+
+
+st.title("Restaurant Name Generator")
+cuisine=st.sidebar.selectbox("Pick a cuisine",("Indian","Italian","Mexican","American","Arabic","French"))
+
+
+
+if cuisine:
+    response=langchain_helper.generate_restaurant_name_and_items(cuisine)
+    st.header(response['restaurant_name'].strip())
+    menu_items=response['menu_items'].strip().split(",")
+    st.write("Menu Items")
+
+    for i in menu_items:
+        st.write("-",i)
